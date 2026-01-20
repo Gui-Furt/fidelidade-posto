e0d65137980836a80823f44fc316e8480eb645ef
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 16:25:38 -0300

    Update transacoes.module.ts

--- backend/src/transacoes.module.ts
+++ backend/src/transacoes.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransacoesService } from './transacoes.service';
 import { TransacoesController } from './transacoes.controller';
-import { ClientesModule } from './clientes.module';
+import { Transacao } from './entities/transacao.entity';
 import { PontosModule } from './pontos.module';
+import { ClientesModule } from './clientes.module';
 
 @Module({
-  imports: [ClientesModule, PontosModule],
+  imports: [
+    TypeOrmModule.forFeature([Transacao]),
+    PontosModule,
+    ClientesModule,
+  ],
   providers: [TransacoesService],
   controllers: [TransacoesController],
-  exports: [TransacoesService], // 👈 ESTA LINHA RESOLVE TUDO
+  exports: [TransacoesService],
 })
 export class TransacoesModule {}
