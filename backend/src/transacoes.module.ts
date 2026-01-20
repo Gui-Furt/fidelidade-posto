3bf6d1b308153cf06870739e263fad046ad69cb2
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 15:34:52 -0300

    Update transacoes.module.ts

--- backend/src/transacoes.module.ts
+++ backend/src/transacoes.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransacoesService } from './transacoes.service';
 import { TransacoesController } from './transacoes.controller';
 import { ClientesModule } from './clientes.module';
+import { PontosModule } from './pontos.module';
 
 @Module({
-  imports: [ClientesModule],
+  imports: [ClientesModule, PontosModule],
   providers: [TransacoesService],
   controllers: [TransacoesController],
 })
