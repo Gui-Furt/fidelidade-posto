ff52aea1fd0ee611e752a50ddfffbd2e187f70f3
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 15:29:14 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { ClientesModule } from './clientes.module';
 import { TransacoesModule } from './transacoes.module';
 import { PontosModule } from './pontos.module';
+import { ApiKeyModule } from './api-key.module';
+import { IntegracaoController } from './integracao.controller';
 
 @Module({
-  imports: [ClientesModule, TransacoesModule, PontosModule],
-  controllers: [AppController],
+  imports: [
+    ClientesModule,
+    TransacoesModule,
+    PontosModule,
+    ApiKeyModule,
+  ],
+  controllers: [AppController, IntegracaoController],
 })
 export class AppModule {}
