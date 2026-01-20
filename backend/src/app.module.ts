ab622cbfa8a9f107535fc3e70653496dddc400f0
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 14:54:49 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ClientesModule } from './clientes.module';
+import { TransacoesModule } from './transacoes.module';
 
 @Module({
-  imports: [ClientesModule],
+  imports: [ClientesModule, TransacoesModule],
   controllers: [AppController],
 })
 export class AppModule {}
