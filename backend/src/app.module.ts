b54912ae5ecac9c88d518e9a30ca7b645e2228dc
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 16:46:17 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+
 import { ClientesModule } from './clientes.module';
 import { TransacoesModule } from './transacoes.module';
 import { PontosModule } from './pontos.module';
 import { ApiKeyModule } from './api-key.module';
-import { IntegracaoController } from './integracao.controller';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
     ClientesModule,
     TransacoesModule,
     PontosModule,
     ApiKeyModule,
   ],
-  controllers: [AppController, IntegracaoController],
+  controllers: [AppController],
 })
 export class AppModule {}
