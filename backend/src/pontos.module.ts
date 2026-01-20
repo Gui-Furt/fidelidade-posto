494425f78dd3c09a173ca61a61acfb2672d5ccef
Gui-Furt <guiiaugusto.ga@gmail.com>
2026-01-20 16:26:55 -0300

    Update pontos.module.ts

--- backend/src/pontos.module.ts
+++ backend/src/pontos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PontosService } from './pontos.service';
 import { PontosController } from './pontos.controller';
+import { Pontos } from './entities/pontos.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pontos])],
   providers: [PontosService],
   controllers: [PontosController],
   exports: [PontosService],
